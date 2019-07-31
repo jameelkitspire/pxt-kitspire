985dd721c25c9b57851b64a6ce62575407e1c32e
jameelkitspire <52672832+jameelkitspire@users.noreply.github.com>
2019-07-31 15:53:23 +0400

    Update robo.ts

--- robo.ts
+++ robo.ts
@@ -1,31 +1,74 @@
-/**
- * Organize your blocks in groups
- */
-//% color="#AA278D"
-//% groups="['Fruits', 'Veggies']"
-namespace food {
-
-    //% block
-    //% group="Veggies"
-    export function potato() {
-
+//% weight=115 color="#FFAB19" icon="K"
+namespace Kitspire{
+    export enum MotorDirection {
+        //% block="Forward"
+        Forward,
+        //% block="Reverse"
+        Reverse
     }
 
-    //% block
-    //% group="Veggies"
-    export function bean() {
-
+    export enum Motors {
+        //%blockId=kitspire_A
+        //% block="motor A"
+        MotorA,
+        //%blockId=kitspire_B
+        //% block="motor B"
+        MotorB
     }
 
-    //% block
-    //% group="Fruits"
-    export function apple() {
+    //% blockId=Kitspire_A
+    //% block="%motor|Direction %dir|Speed %speed"
+    //% speed.min=0 speed.max=100
+    export function motorOn(motor: Motors, dir: MotorDirection, speed: number): void {
+        let OutputVal = Math.clamp(0, 100, speed) * 10;
 
+        switch (motor) {
+            case Motors.MotorA: 
+                switch (dir) {
+                    case MotorDirection.Forward:
+                        pins.analogWritePin(AnalogPin.P0, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P1, 0); 
+                           break
+                    case MotorDirection.Reverse:
+                        pins.analogWritePin(AnalogPin.P1, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P0, 0);
+                        break
+                }
+
+                break;
+            case Motors.MotorB:
+            switch (dir) {
+                    case MotorDirection.Forward:
+                        pins.analogWritePin(AnalogPin.P0, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P16, 0); 
+                        break
+                    case MotorDirection.Reverse:
+                        pins.analogWritePin(AnalogPin.P16, OutputVal);
+                        pins.digitalWritePin(DigitalPin.P0, 0);
+                        break
+                }
+
+                break;
+        }
+    }
+    //% blockId=kitspir_Off
+    //%block="Turn off %motor"
+    export function motorOff(motor: Motors): void {
+        switch (motor) {
+            case Motors.MotorA:
+                pins.digitalWritePin(DigitalPin.P8, 0);
+                pins.digitalWritePin(DigitalPin.P12, 0);
+                break
+            case Motors.MotorB:
+                pins.digitalWritePin(DigitalPin.P0, 0);
+                pins.digitalWritePin(DigitalPin.P16, 0);
+                break
+        }
     }
 
-    //% block
-    //% group="Fruits"
-    export function banana() {
-
-    }
 }
+basic.forever(function () {
+    Kitspire.motorOn(Kitspire.Motors.MotorA, Kitspire.MotorDirection.Forward, 100)
+})
+
+
